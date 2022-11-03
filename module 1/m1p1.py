1304828d47e4983bf17623886e413f2364d8d951
JoelEngstrom <joel.n.engstrom@protonmail.com>
2022-11-03 14:25:31 +0100

    m1p1 luhn done

--- module 1/m1p1.py
+++ module 1/m1p1.py
@@ -1,1 +1,23 @@
-print("Initial commit")
+from hashlib import sha256
+
+def int_to_hex(num: int) -> bytes:
+  return num.to_bytes(4, byteorder="big")
+
+def int_to_arr(num: int) -> list:
+  return ""
+
+def hex_to_int(s: str) -> int:
+  return int.from_bytes(s.encode(), byteorder="big")
+
+def hex_to_arr(hex: bytes) -> list:
+  return ""
+
+def arr_to_int(arr: list) -> int:
+  return ""
+
+def arr_to_hex(arr: list) -> bytes:
+  return ""
+
+def sha(arr: list) -> int:
+  b = sha256(arr).digest()
+  return int.from_bytes(b, byteorder="big")
---
+++ module 1/m1p1_luhn.py
@@ -0,0 +1,37 @@
+def luhn(s: str) -> int:
+  x_idx = s[::-1].index("X")
+  s = s.replace("X", "0")
+  possible_x = 10 - sum(double_nums(s)) % 10
+  if possible_x == 10: possible_x = 0
+  if x_idx % 2 == 0:
+    return possible_x
+  elif possible_x % 2 == 0:
+    return int(possible_x / 2)
+  else:
+    x = "".join([str(1), str(possible_x - 1)])
+    return int(int(x) / 2)
+
+def double_nums(s: str) -> tuple[list, int]:
+  "Doubles every other number. Returns a reverted array"
+  new_num = []
+  for idx, c in enumerate(s[::-1]):
+    if idx % 2 == 1:
+      new_c = int(c) * 2
+      new_c = shorten_num(new_c)
+      new_num.append(new_c)
+    else: new_num.append(int(c))
+  return new_num
+
+def shorten_num(i: int) -> int:
+  "Add together all numbers in an int"
+  s = str(i)
+  new_num = 0
+  for c in s: new_num += int(c)
+  return new_num
+
+f = open("m1p1_luhn.in", "r")
+s = f.read()
+out = "".join([str(luhn(line)) for line in s.split("\n")])
+print(out)
+print(int(out) == 5496331440914992338434701218071555719657419448245128019889398009532562488698540071959143506293615978)
+f.close()
